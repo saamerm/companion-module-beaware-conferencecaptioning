638fc40ab7a4abd64ab7523b1c1b0909ef386ee4
Saamer Mansoor <i@saamer.me>
2025-07-22 19:05:29 -0400

    Fixed error

--- src/actions.ts
+++ src/actions.ts
@@ -1,12 +1,12 @@
 import type { ModuleInstance } from './main.js'
 import { InstanceStatus } from '@companion-module/base'
 // const constants = require('./constants')
-import { CONFCAPCOMMANDS, LANGUAGES } from './constants.js'
+import { CONFCAPCOMMANDS } from './constants.js'
 
 export function UpdateActions(self: ModuleInstance): void {
 	self.setActionDefinitions({
 		get_caption: {
-			name: 'Test Connection',
+			name: 'Test Stream Connection',
 			options: [
 				{
 					id: 'startTranscribing',
@@ -17,8 +17,39 @@ export function UpdateActions(self: ModuleInstance): void {
 				},
 			],
 			callback: async (event) => {
-				const response = await fetch(`https://api.deafassistant.com/stream/LiteGetStream?streamName=${self.config.streamName}`, {
-					method: 'POST',
+				console.log('get_caption action triggered', event)
+				const response = await fetch(
+					`https://api.deafassistant.com/stream/LiteGetStream?streamName=${self.config.streamName}`,
+					{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					},
+				)
+				if (response.status == 200) {
+					self.updateStatus(InstanceStatus.Ok)
+				} else {
+					self.updateStatus(InstanceStatus.ConnectionFailure)
+				}
+			},
+		},
+		get_localhost: {
+			name: 'Test Localhost Connection',
+			options: [
+				{
+					id: 'showCaptions',
+					type: 'dropdown',
+					label: 'Action type',
+					choices: CONFCAPCOMMANDS,
+					default: 'showCaptions',
+				},
+			],
+			callback: async (event) => {
+				console.log('get_localhost action triggered', event)
+				const apiUrl = self.config.localhostUrl.replace('index.html', 'stream/LiteGetStream')
+				const response = await fetch(apiUrl, {
+					method: 'GET',
 					headers: {
 						'Content-Type': 'application/json',
 					},
@@ -30,31 +61,5 @@ export function UpdateActions(self: ModuleInstance): void {
 				}
 			},
 		},
-		name: 'Test Localhost Url',
-		options: [
-			{
-				id: 'startTranscribing',
-				type: 'dropdown',
-				label: 'Action type',
-				choices: CONFCAPCOMMANDS,
-				default: 'startTranscribing',
-			},
-		],
-		callback: async (event) => {
-			var apiUrl = self.config.localhostUrl.replace("index.html", "stream/LiteGetStream"); //http://10.0.0.199:8080/index.html
-
-			const response = await fetch(apiUrl, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			})
-			if (response.status == 200) {
-				self.updateStatus(InstanceStatus.Ok)
-			} else {
-				self.updateStatus(InstanceStatus.ConnectionFailure)
-			}
-		},
-	}
 	})
 }
